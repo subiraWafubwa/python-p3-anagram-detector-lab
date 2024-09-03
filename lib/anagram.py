4a237b53cf6947287e153f07a9bd0cbdecd7ed79
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-09-03 09:50:32 +0300

    Passed all tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, word_list):
+        return [word for word in word_list if sorted(word.lower()) == self.sorted_word]
